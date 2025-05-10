783847bf325bed6b9289d6feeb2b1e059befef1b
Yayo_joaco <gjrabanal@gmail.com>
2025-05-09 20:18:24 -0500

    first commit

---
+++ src/main/java/com/example/lab4_20221957/Lab420221957Application.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20221957;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab420221957Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab420221957Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lab4_20221957/Lab420221957ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20221957;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab420221957ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
